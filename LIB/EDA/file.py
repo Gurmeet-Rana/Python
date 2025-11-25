79058a0fd234d28ade5a2de17735eb171e35a570
Gurmeet-Rana <ranagur361@gmail.com>
2025-11-25 15:10:46 +0530

    END TERM EXAMINATION ML 2025 November

---
+++ LIB/EDA/file.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt 
+#df = pd.read_csv("train.csv")
+#df.head() 
+ # STEP 1 : ANALYSE COLUMN TYPES 
+ # STEP 2 : UNIVARIATE ANALYSIS : {Descriptive Statistics , Visualizations , Identifying Outliers , Skewness , Conclusion}, bivariate ANALYSIS , MULTI-VARIATE ANALYSIS 
+
+#df['col'].describe() 
+
+
+ # STEP 3 :  
---
+++ endTermML.py
@@ -0,0 +1,14 @@
+""" import numpy as np 
+import matplotlib.pyplot as plt
+import pandas as pd """
+
+str1=input("Enter first string : ")
+str2=input("Enter second string : ")
+res=f"{str1} {str2}"
+print(res)
+
+temp=' fav '
+print(temp.strip(),'kafjdk')
+print(temp.rstrip(),'adfad')
+print(temp.lstrip(),'a')
+print(temp.trim())
